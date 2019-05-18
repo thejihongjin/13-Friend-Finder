45202b3cb3a0a536d89f1d0c0e46d1ab373e2c4e
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-05-18 13:08:05 -0700

    added initial files

---
+++ server.js
@@ -0,0 +1,2 @@
+// 1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+// 2. Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
