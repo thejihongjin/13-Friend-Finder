e413b0c7d59d15af03bdca4a018cedcd79f8be47
unknown <1469406888@mil>
2019-05-23 11:56:04 -0700

    initialized friendsArr to empty arr

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,3 @@
-var friendsArr = [ //set to empty array
-    {
-        name: "G Unit",
-        photo: "",
-        scores: [] // fill in after come up with survey questions
-    }
-]
+var friendsArr = []
 
 module.exports = friendsArr;
