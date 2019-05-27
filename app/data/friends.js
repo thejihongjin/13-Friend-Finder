1cb7138ba07c964e95d80276f310704d93939565
thejihongjin <46304180+thejihongjin@users.noreply.github.com>
2019-05-26 18:18:11 -0700

    added more friends to starting array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friendsArr = [
         name: "Jihong",
         photo: "https://via.placeholder.com/150",
         scores: [5, 3, 5, 4, 5, 3, 3, 4, 1, 3]
+    },
+    {
+        name: "Somer",
+        photo: "https://via.placeholder.com/150",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "Corey",
+        photo: "https://via.placeholder.com/150",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     }
 ]
 
